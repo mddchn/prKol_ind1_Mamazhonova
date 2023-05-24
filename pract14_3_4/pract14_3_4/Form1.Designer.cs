e8e571bdd1ecd1d4da6846284e863e7dc45083c8
mddchn <93264667+mddchn@users.noreply.github.com>
2023-05-24 14:32:30 +0500

    Add files via upload

---
+++ pract14_3_4/pract14_3_4/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace pract14_3_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить очередь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Разгрузить очередь";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(28, 169);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(173, 23);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите количество n";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(244, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 199);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(486, 41);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(261, 199);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Младше 40 лет";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Старше 40 лет";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(244, 246);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(261, 199);
+            this.listBox3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ pract14_3_4/pract14_3_4/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pract14_3_4
+{
+    public partial class Form1 : Form
+    {
+        string filename = "people.txt";
+        string filename2 = "people2.txt";
+        private Queue<int> queue;
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = (int)numericUpDown1.Value; 
+            for (int i = 1; i <= n; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            MessageBox.Show("Очередь заполнена!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (queue.Count == 0) 
+            {
+                MessageBox.Show("Очередь пуста!");
+                return;
+            }
+
+            int number = queue.Dequeue(); 
+            MessageBox.Show("Из очереди извлечено число: " + number);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Queue<int> numbers = new Queue<int>();
+            string[] lines = File.ReadAllLines(filename2);
+
+
+            foreach (string line2 in lines)
+            {
+                string[] parts = line2.Split(' ');
+
+                numbers.Enqueue(int.Parse(parts[3]));
+                if (numbers.Dequeue() >= 1)
+                {
+                    if (numbers.Dequeue() < 40)
+                    {
+                        listBox1.Items.Add(line2);
+                    }
+                    else
+                    {
+                        listBox2.Items.Add(line2);
+                    }
+                }
+                else Console.WriteLine("Ошибка!");
+            }
+            Metod();
+        }
+        public void Metod()
+        {
+            queue = new Queue<int>();
+
+
+
+            string[] lines2 = File.ReadAllLines(filename);
+
+
+            List<Person> people = new List<Person>();
+
+            foreach (string line in lines2)
+            {
+                string[] parts = line.Split(' ');
+
+                string lastName = parts[0];
+                string firstName = parts[1];
+                string middleName = parts[2];
+                int age = int.Parse(parts[3]);
+                int weight = int.Parse(parts[4]);
+
+                Person person = new Person(lastName, firstName, middleName, age, weight);
+                people.Add(person);
+            }
+
+
+            people = people.OrderBy(p => p.Age).ToList();
+
+
+            foreach (Person person in people)
+            {
+                string line = $"{person.LastName} {person.FirstName} {person.MiddleName} {person.Age} {person.Weight}";
+                listBox3.Items.Add(line);
+            }
+
+        }
+        public class Person
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public int Age { get; set; }
+            public int Weight { get; set; }
+
+            public Person(string lastName, string firstName, string middleName, int age, int weight)
+            {
+                LastName = lastName;
+                FirstName = firstName;
+                MiddleName = middleName;
+                Age = age;
+                Weight = weight;
+            }
+        }
+    }
+}
